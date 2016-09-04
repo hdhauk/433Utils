187d2447a9ba87e7ad6f8adcf69c96955ff4fbc0
halvor <halvor@mac-15.local>
2016-09-04 12:31:40 +0200

    Removed "Received" prefix

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -11,19 +11,19 @@
 #include "../rc-switch/RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
-     
-     
+
+
 RCSwitch mySwitch;
- 
+
 
 
 int main(int argc, char *argv[]) {
-  
+
      // This pin is not the first pin on the RPi GPIO header!
      // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      // for more information.
      int PIN = 2;
-     
+
      if(wiringPiSetup() == -1) {
        printf("wiringPiSetup failed, exiting...");
        return 0;
@@ -35,30 +35,29 @@ int main(int argc, char *argv[]) {
      mySwitch = RCSwitch();
      if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
      mySwitch.enableReceive(PIN);  // Receiver on interrupt 0 => that is pin #2
-     
-    
+
+
      while(1) {
-  
+
       if (mySwitch.available()) {
-    
+
         int value = mySwitch.getReceivedValue();
-    
+
         if (value == 0) {
           printf("Unknown encoding\n");
-        } else {    
-   
-          printf("Received %i\n", mySwitch.getReceivedValue() );
+        } else {
+
+          printf( mySwitch.getReceivedValue() );
         }
-    
+
         mySwitch.resetAvailable();
-    
+
       }
-      
-  
+
+
   }
 
   exit(0);
 
 
 }
-
