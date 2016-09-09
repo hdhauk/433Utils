45c0372232e9523a111b416fd4838d2f58298d95
hdhauk <hh@macbook.com>
2016-09-09 19:15:20 +0200

    Updated gitignore

--- RPi_utils/codesend.cpp
+++ RPi_utils/codesend.cpp
@@ -5,30 +5,32 @@ protocol    - According to rc-switch definitions
 pulselength - pulselength in microseconds
 
  'codesend' hacked from 'send' by @justy
- 
+
  - The provided rc_switch 'send' command uses the form systemCode, unitCode, command
-   which is not suitable for our purposes.  Instead, we call 
+   which is not suitable for our purposes.  Instead, we call
    send(code, length); // where length is always 24 and code is simply the code
    we find using the RF_sniffer.ino Arduino sketch.
 
-(Use RF_Sniffer.ino to check that RF signals are being produced by the RPi's transmitter 
+(Use RF_Sniffer.ino to check that RF signals are being produced by the RPi's transmitter
 or your remote control)
 */
 #include "../rc-switch/RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
-     
+
 
 int main(int argc, char *argv[]) {
-    
+
     // This pin is not the first pin on the RPi GPIO header!
     // Consult https://projects.drogon.net/raspberry-pi/wiringpi/pins/
     // for more information.
-    int PIN = 0;
-    
-    // Parse the first parameter to this command as an integer
-    int protocol = 0; // A value of 0 will use rc-switch's default value
-    int pulseLength = 0;
+    int PIN;
+    if (argv[1] != NULL) {
+      PIN = atoi(argv[1]);
+    }else{
+      int PIN = 0; // PIN: #18 on breakout board
+    }
+
 
     // If no command line argument is given, print the help text
     if (argc == 1) {
@@ -41,18 +43,16 @@ int main(int argc, char *argv[]) {
 
     // Change protocol and pulse length accroding to parameters
     int code = atoi(argv[1]);
-    if (argc >= 3) protocol = atoi(argv[2]);
-    if (argc >= 4) pulseLength = atoi(argv[3]);
-    
+
     if (wiringPiSetup () == -1) return 1;
     printf("sending code[%i]\n", code);
     RCSwitch mySwitch = RCSwitch();
     if (protocol != 0) mySwitch.setProtocol(protocol);
     if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
     mySwitch.enableTransmit(PIN);
-    
+
     mySwitch.send(code, 24);
-    
+
     return 0;
 
 }
